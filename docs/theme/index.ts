86341f786e5e9ab13d4e981dc4c21f18cc53cc34
yian2909 <915826506@qq.com>
2024-10-08 21:14:40 +0800

    Delete docs/theme directory

--- docs/theme/index.ts
+++
@@ -1,8 +0,0 @@
-import DefaultTheme from 'vitepress/theme'
-import 'vitepress-theme-demoblock/dist/theme/styles/index.css'
-import './styles/var.css'
-
-export default {
-    ...DefaultTheme,
-    enhanceApp({ app }){}
-}
